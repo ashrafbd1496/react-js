7ade224625da5ca6d6a2872d6130eb9f9a46600b
ashrafbd1496 <asraf1496@gmail.com>
2024-06-23 06:47:23 +0600

    normal way added one more counter

--- react-sumit/main.js
+++ react-sumit/main.js
@@ -1,10 +1,17 @@
 // import React from "react";
 // import ReactDOM from "react-dom/client";
 let number = 0;
-const incrementBtn = document.getElementById('button')
-const display = document.getElementById('display')
+let number2 = 0;
+const incrementBtn = document.getElementById("button");
+const incrementBtn2 = document.getElementById("button2");
+const display = document.getElementById("display");
+const display2 = document.getElementById("display2");
 
-incrementBtn.addEventListener('click', ()=>{
-    number++;
-    display.textContent = number;
-})
+incrementBtn.addEventListener("click", () => {
+  number++;
+  display.textContent = number;
+});
+incrementBtn2.addEventListener("click", () => {
+  number2++;
+  display2.textContent = number2;
+});
