6e7a2e9cfa4c410767f3ced8040cb1630d7102a5
ashrafbd1496 <asraf1496@gmail.com>
2024-06-26 12:49:51 +0600

    render variable

--- react-old-way/src/App.js
+++ react-old-way/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>React js by <span>{firstName + LastName}</span></h1>
+        <h1>Today <span>{new Date().toLocaleString()}</span></h1>
       </header>
     </div>
   );
 }
+const firstName = "Jahid ";
+const LastName = "Hasan";
+const element = (
+  <div>
+    <h1>HEllo React</h1>
+  </div>
+);
+let element2 = <div>lorem ipsum</div>;
+//console.log(element);
+// const rootCont = document.getElementById("root");
+// const root = ReactDOM.createRoot(rootCont);
+//root.render(element);
 
 export default App;
