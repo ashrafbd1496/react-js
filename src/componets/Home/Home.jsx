b26188e0942a5d7625dc3a264efb79229e1b5164
ashrafbd1496 <asraf1496@gmail.com>
2024-06-28 14:23:25 +0600

    added time using setInerval function

--- src/componets/Home/Home.jsx
+++ src/componets/Home/Home.jsx
@@ -1,10 +1,42 @@
 import "./Home.css";
+import React, { useState, useEffect } from "react";
 
-function Home(){
-    return(
-        <div>
-            <h1 className="heading">Hello from Home component</h1>
-        </div>
-    )
+function Home() {
+  const [timeNow, setTimeNow] = useState(new Date().toLocaleTimeString());
+  // Effect to update the time every second
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTimeNow(new Date().toLocaleTimeString());
+    }, 1000);
+
+    // Cleanup function to clear the interval when the component unmounts
+    return () => clearInterval(intervalId);
+  }, []);
+
+  /*  
+    element = {
+        type: 'h1',
+        props: {
+            className: 'heading'
+            tabIndex: 0,
+            children: 'Allahu AKbar'
+        }
+    }
+    */
+  //write jsx and pass parameter by sumit
+  function getGreeting(user) {
+    //user is parameter
+    if (user) {
+      return <h3>Name {user}! is changable</h3>;
+    }
+    return <h3>Hello Stranger</h3>;
+  }
+  const element2 = <h3>Hello {getGreeting("Ashraf")}</h3>; //remove argument asrhaf -Stranger will show
+  return (
+    <div>
+      {element2}
+      <span className="time-now">{timeNow}</span>
+    </div>
+  );
 }
 export default Home;
