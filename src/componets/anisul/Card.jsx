3bda16fb372986c096ebbd307604a398a7f21aa8
ashrafbd1496 <asraf1496@gmail.com>
2024-07-04 18:39:23 +0600

    created card image properties and send directly

--- src/componets/anisul/Card.jsx
+++ src/componets/anisul/Card.jsx
@@ -1,22 +1,28 @@
 import React from "react";
 import "./style.css";
 
-const Card = (props) => {
-  //we can catch those attributes using props
+const Card = ({ CardTitle, CardDesc ,CardImage}) => {
+  //we destructured properties CardTitle, CardDesc from props object directly
+  //we can catch those properties using props
   //console.log(props);
+  // const {CardTitle, CardDesc} = props;
+  /**
+   * if the property is an arrray like
+   * const name = ['dog', 'cat'];
+   * we can write it like  const [dog, cat] = name;
+   */
   return (
     <div>
       <div className="card">
         <img
-          src="/public/card-img-1.png"
+          src={CardImage}
           alt="Card Image"
           className="card-image"
         />
         <div className="card-content">
-          <h2 className="card-title">{props.CardTitle}</h2>
-          <p className="card-description">
-           {props.CardDesc}
-          </p>
+          <h2 className="card-title">{CardTitle}</h2>
+          {/* <h2 className="card-title">{props.CardTitle}</h2> */}
+          <p className="card-description">{CardDesc}</p>
           <a href="#" className="card-button">
             Read More
           </a>
--- src/componets/anisul/ReactProps.jsx
+++ src/componets/anisul/ReactProps.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
-import Card from './Card';
-import './style.css';
+import React from "react";
+import Card from "./Card";
+import "./style.css";
 
 const ReactProps = () => {
   return (
-    <div className='card-container'>
-        <Card CardTitle="Card Title 1" CardDesc = "desc -1 Lorem ipsum dollar"/>
-        <Card CardTitle="Card Title 2" CardDesc = "desc -2 Lorem ipsum dollar"/>
-        <Card CardTitle="Card Title 2" CardDesc = "desc -3 Lorem ipsum dollar"/>
+    <div className="card-container">
+      <Card
+        CardTitle="Card Title 1"
+        CardDesc="desc -1 we destructured properties CardTitle, CardDesc from props object directly"
+        CardImage="/public/card-img-1.png"
+      />
+      <Card
+        CardTitle="Card Title 2"
+        CardDesc="desc -2 we can catch those properties using props"
+        CardImage="/public/card-img-2.png"
+      />
+      <Card
+        CardTitle="Card Title 2"
+        CardDesc="desc -3 const {CardTitle, CardDesc} = props;"  CardImage="/public/card-img-1.png"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default ReactProps
+export default ReactProps;
