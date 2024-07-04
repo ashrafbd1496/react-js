d98789c8a1cf683e8779cb2ce3f9a575452873dd
ashrafbd1496 <asraf1496@gmail.com>
2024-07-04 17:54:24 +0600

    Catched Card attributes using props

--- src/componets/anisul/Card.jsx
+++ src/componets/anisul/Card.jsx
@@ -1,19 +1,30 @@
-import React from 'react';
-import './style.css';
+import React from "react";
+import "./style.css";
 
-const Card = () => {
+const Card = (props) => {
+  //we can catch those attributes using props
+  console.log(props);
   return (
     <div>
       <div className="card">
-        <img src="/public/card-img-1.png" alt="Card Image" class="card-image"/>
+        <img
+          src="/public/card-img-1.png"
+          alt="Card Image"
+          className="card-image"
+        />
         <div className="card-content">
-            <h2 className="card-title">Card Title</h2>
-            <p className="card-description">This is a description of the card. It can hold various types of content, including text, images, and more.</p>
-            <a href="#" className="card-button">Read More</a>
+          <h2 className="card-title">Card Title</h2>
+          <p className="card-description">
+            This is a description of the card. It can hold various types of
+            content, including text, images, and more.
+          </p>
+          <a href="#" className="card-button">
+            Read More
+          </a>
         </div>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
