5b88d1fe2fe5b96af2771741a232902f8a1c372d
ashrafbd1496 <asraf1496@gmail.com>
2024-07-09 12:04:41 +0600

    added react icon

--- src/componets/anisul/NestedMapping.jsx
+++ src/componets/anisul/NestedMapping.jsx
@@ -1,43 +1,43 @@
-import React from 'react';
-import './style.css';
+import React from "react";
+import { FaListAlt } from "react-icons/fa";
+import "./style.css";
 
 const users = [
-    {
-        fullName: 'Ashraf',
-        age: 32,
-        phones: [
-            { home: '01558917333' },
-            { office: '01911967727' }
-        ]
-    },
-    {
-        fullName: 'Sumit',
-        age: 30,
-        phones: [
-            { home: '01458955' },
-            { office: '0215489' }
-        ]
-    },
+  {
+    fullName: "Ashraf",
+    age: 32,
+    phones: [{ home: "01558917333" }, { office: "01911967727" }],
+  },
+  {
+    fullName: "Sumit",
+    age: 30,
+    phones: [{ home: "01458955" }, { office: "0215489" }],
+  },
 ];
 
 const NestedMapping = () => {
-    return (
-        <div>
-            <h1>Nested List</h1>
-            {users.map((user, userIndex) => (
-                <article key={userIndex}>
-                    <h3>{user.fullName}</h3>
-                    <p>{user.age}</p>
-                    {user.phones.map((phone, phoneIndex) => (
-                        <div key={phoneIndex}>
-                            {phone.home && <p>Home: {phone.home}</p>}
-                            {phone.office && <p>Office: {phone.office}</p>}
-                        </div>
-                    ))}
-                </article>
-            ))}
-        </div>
-    );
-}
+  return (
+    <div>
+      <h1>
+        <span className="icon">
+          <FaListAlt />
+        </span>
+        Nested List
+      </h1>
+      {users.map((user, userIndex) => (
+        <article key={userIndex}>
+          <h3>Full Name: {user.fullName}</h3>
+          <p>Age: {user.age}</p>
+          {user.phones.map((phone, phoneIndex) => (
+            <div key={phoneIndex}>
+              {phone.home && <p>Home: {phone.home}</p>}
+              {phone.office && <p>Office: {phone.office}</p>}
+            </div>
+          ))}
+        </article>
+      ))}
+    </div>
+  );
+};
 
 export default NestedMapping;
